8df2e023c73567413c4a7cbe456f8757d3d45c8a
oicjacky <610611105@gms.ndhu.edu.tw>
2020-02-12 20:18:42 +0800

    modified: APLM hw03&04

--- APLM hw03/APLM hw04.R
+++ APLM hw03/APLM hw04.R
@@ -1,5 +1,5 @@
 library(stringr)
-setwd("C:/Users/Master/Desktop/黃三/應用統計線性模型")
+setwd("D:/github_oicjacky/Applied Statistical Linear Model/APLM hw03")
 rm(list = ls())
 data <- read.table("CH09TA01.txt")
 colnames(data) <- c("X1","X2","X3","X4","X5","Y","lnY")
@@ -28,7 +28,7 @@ for (j in 1:length(candidate) ) {
   
   pred.value <- rep(0 ,n)
   for(i in 1:n){
-    d <- combn(1:n ,1)[,i] # CV(1)
+    d <- combn(1:n ,1)[,i] # CV(1) or leave-one-out cross validation
     data_train <- data[-d ,]
     data_test <- data[ d ,]
     # training
